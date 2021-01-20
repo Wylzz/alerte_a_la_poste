10e574aed5851b9b9b7e272e39ef5782ad6bf684
Djamel <djamel.wrk@gmail.com>
2021-01-20 10:41:02 +0100

    final

--- projet_1.py
+++ projet_1.py
@@ -9,7 +9,7 @@ def log(file):
         a = x.split(";")
         b = a[2].split(" ")
         hours, minutes = map(int, b[1].split(':'))
-        if hours > 19:
+        if hours > 19 or hours < 8:
             # print(b[1])
             print(a[1])
             print(a[0])
@@ -30,49 +30,8 @@ def warn(txt, logg):
             if h == a[0]:
 
                 o = open("suspect.txt", "a")
-                o.write(a[1] + ";" + "\n")
+                o.write(a[1] + ";" + str(h.count(a[0])) + "\n")
                 print(a[1])
 
+
 warn(warning, connexion)
-
-'''o = open("suspect.txt", "w+")
-o.write(str(warn(warning, connexion)))'''
-
-'''
-for x in z:
-    v = x.split
-    print(x)'''
-
-'''for x in range(0, 11):
-    for v in z:
-        a = v.replace(str(x), '')
-        print(a)'''
-
-'''for x in z:
-    a = x.replace(".", "")
-    b = a.replace("1", "")
-    c = b.replace("2", "")
-    d = c.replace("3", "")
-    e = d.replace("4", "")
-    f = e.replace("5", "")
-    g = f.replace("6", "")
-    h = g.replace("7", "")
-    i = h.replace("8", "")
-    j = i.replace("9", "")
-    k = j.replace("10", "")
-    l = k.replace("0", "")
-    m = l.replace("/", "")
-    o = m.replace(":", "")
-    print(o.replace(";", ""))'''
-
-'''str = open('connexion.log', 'r')
-print(f.read())
-
-z = list(str)
-for x in z:
-    print(x)
-
-    with open('connexion.log', 'r') as f:
-    mylist = [line.rstrip('\n') for line in f]
-
-    print(mylist)'''
